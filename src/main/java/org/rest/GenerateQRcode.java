da09fa49bf709571c0d9e2488a15f4d2a43e8f34
TimurSatarov <timursatarov3@gmail.com>
2023-10-26 17:13:44 +0200

    initial commit

---
+++ src/main/java/org/rest/GenerateQRcode.java
@@ -0,0 +1,26 @@
+package org.rest;
+
+import javax.swing.*;
+import java.awt.*;
+public class GenerateQRcode {
+
+
+    private static void createAndShowGUI() {
+        JPanel panel = new JPanel();
+        JFrame frame = new JFrame("QR Code Generate");
+
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setSize(new Dimension(300,300));
+
+
+        frame.add(panel);
+
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            createAndShowGUI();
+        });
+    }
+}
